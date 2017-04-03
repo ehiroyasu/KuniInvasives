72303510b43076d0f5f127b7aac8217d95dc7189
ehiroyasu <ehiroyasu@bren.ucsb.edu>
2017-04-03 14:38:20 -0700

    plotting by party id

--- analysis_meansupport.R
+++ analysis_meansupport.R
@@ -23,22 +23,59 @@ summary(control_mean_strength <- mean(support_oppose[treat_cat=="control"], na.r
 #BY PARTY ID#
 
 #Among Republicans
-summary(ecogain_mean <- mean(support_binary[republican==1], na.rm=T)) #0.764 is overall number
-summary(ecogain_mean <- mean(support_binary[treat_cat=="ecogain" & republican==1], na.rm=T)) #0.793
-summary(ecoloss_mean <- mean(support_binary[treat_cat=="ecoloss" & republican==1], na.rm=T)) #0.833
-summary(econgain_mean <- mean(support_binary[treat_cat=="econgain" & republican==1], na.rm=T)) #0.732
-summary(econloss_mean <- mean(support_binary[treat_cat=="econloss" & republican==1], na.rm=T)) #0.793
-summary(control_mean <- mean(support_binary[treat_cat=="control" & republican==1], na.rm=T)) #0.660
+summary(rep_overall <- support_binary[republican==1], na.rm=T) #0.764 is overall number
+summary(rep_ecogain <- support_binary[treat_cat=="ecogain" & republican==1], na.rm=T) #0.793
+summary(rep_ecoloss <- support_binary[treat_cat=="ecoloss" & republican==1], na.rm=T) #0.833
+summary(rep_econgain <- support_binary[treat_cat=="econgain" & republican==1], na.rm=T) #0.732
+summary(rep_econloss <- support_binary[treat_cat=="econloss" & republican==1], na.rm=T) #0.793
+summary(rep_control <- support_binary[treat_cat=="control" & republican==1], na.rm=T) #0.660
 #Ordering: eco-loss, econ-loss & eco-gain (tied), econ-gain, control
 
+#plotting republicans
+m = c(mean(rep_control), mean(rep_econgain), mean(rep_econloss), mean(rep_ecogain), mean(rep_ecoloss))
+treat_cat_rep = c("Control", "Econ Gain", "Econ Loss", "Ecol Gain", "Ecol Loss")
+se = c(sd(rep_control)/sqrt(length(rep_control)), sd(rep_econgain)/sqrt(length(rep_econgain)), 
+       sd(rep_econloss)/sqrt(length(rep_econloss)), sd(rep_ecogain)/sqrt(length(rep_ecogain)),
+       sd(rep_ecoloss)/sqrt(length(rep_ecoloss)))
+party_rep<-as.factor(rep(1, each=length(m)))
+#creating a separate data frame with republican data
+rep.df<-data.frame(treat_cat_rep, m, se, party_rep)
+rep.df<-setNames(rep.df, c("treat_cat", "mean", "se", "partyid"))
+
+
 #Among Democrats
-summary(overall_mean <- mean(support_binary[democrat==1], na.rm=T)) #0.697 is overall number
-summary(ecogain_mean <- mean(support_binary[treat_cat=="ecogain" & democrat==1], na.rm=T)) #0.733
-summary(ecoloss_mean <- mean(support_binary[treat_cat=="ecoloss" & democrat==1], na.rm=T)) #0.885
-summary(econgain_mean <- mean(support_binary[treat_cat=="econgain" & democrat==1], na.rm=T)) #0.643
-summary(econloss_mean <- mean(support_binary[treat_cat=="econloss" & democrat==1], na.rm=T)) #0.702
-summary(control_mean <- mean(support_binary[treat_cat=="control" & democrat==1], na.rm=T)) #0.558
+summary(dem_overall <- support_binary[democrat==1], na.rm=T) #0.697 is overall number
+summary(dem_ecogain <- support_binary[treat_cat=="ecogain" & democrat==1], na.rm=T) #0.733
+summary(dem_ecoloss <- support_binary[treat_cat=="ecoloss" & democrat==1], na.rm=T) #0.885
+summary(dem_econgain <- support_binary[treat_cat=="econgain" & democrat==1], na.rm=T) #0.643
+summary(dem_econloss <- support_binary[treat_cat=="econloss" & democrat==1], na.rm=T) #0.702
+summary(dem_control <- support_binary[treat_cat=="control" & democrat==1], na.rm=T) #0.558
 #Ordering: eco-loss, eco-gain, econ-loss, econ-gain, control
+
+m = c(mean(dem_control), mean(dem_econgain), mean(dem_econloss), mean(dem_ecogain), mean(dem_ecoloss))
+treat_cat_dem = c("Control", "Econ Gain", "Econ Loss", "Ecol Gain", "Ecol Loss")
+se = c(sd(dem_control)/sqrt(length(dem_control)), sd(dem_econgain)/sqrt(length(dem_econgain)), 
+       sd(dem_econloss)/sqrt(length(dem_econloss)), sd(dem_ecogain)/sqrt(length(dem_ecogain)),
+       sd(dem_ecoloss)/sqrt(length(dem_ecoloss)))
+party_dem<-as.factor(rep(2, length(m)))
+#creating a separate data frame with republican data
+dem.df<-data.frame(treat_cat_dem, m, se, party_dem)
+dem.df<-setNames(dem.df, c("treat_cat", "mean", "se", "partyid"))
+
+
+#plotting dems and reps together
+library(ggplot2)
+theme_update(plot.title = element_text(hjust = 0.5))
+
+party.df<-rbind.data.frame(rep.df, dem.df)
+ggplot(party.df, aes(x=treat_cat, y=mean, fill=partyid)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se),
+                width=.2, position=position_dodge(.9)) +
+  ylim(0,1)+xlab("Treatment Category") + ylab("Mean")+
+  scale_fill_hue(name="Party", labels=c("Republican", "Democrat")) +
+  ggtitle("Republicans vs Democrats")
+
 
 #Among Unaffiliated
 summary(overall_mean <- mean(support_binary[independent==1], na.rm=T)) #0.746 is overall number
